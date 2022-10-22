363a5a91e9ec6da2476c14f9dad28463d5984124
Felix Rojas <habbowalkvps@gmail.com>
2022-10-22 14:01:52 -0500

    Update vistas.py

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -48,12 +48,5 @@ class VistaLogIn(Resource):
         message = client.messages.create(to='whatsapp:+573194625339', 
                                         from_='whatsapp:+14155238886',
                                 body='Hola :), vimos que no completaste el proceso para obtener tu tarjeta de crédito Aqua BBVA, ¿deseas hacer el proceso por este medio?')
-        usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
-                                       Usuario.contrasena == request.json["contrasena"]).first()
-        db.session.commit()
-        if usuario is None:
-            return "El usuario o contraseña es incorrecto.", 404
-        else:
-            token_de_acceso = create_access_token(identity=usuario.id)
-            return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso}
+        return "Enviando mensaje...", 200
 

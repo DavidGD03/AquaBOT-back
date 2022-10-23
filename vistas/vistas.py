81acb032269d356ec03d51d607670c1b2e02f609
Felix Rojas <habbowalkvps@gmail.com>
2022-10-22 23:38:55 -0500

    Update vistas.py

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -13,8 +13,6 @@ from vistas import config
 
 usuario_schema = UsuarioSchema()
 
-
-
 AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
 REGION_NAME = config.REGION_NAME
